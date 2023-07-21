0ff7ad429cf1e26297191efc3598045b74cad1f3
Leighton Grant <oneal.grant@outlook.com>
2023-07-21 08:03:43 +0100

    Updates social links

--- src/components/social/Social.js
+++ src/components/social/Social.js
@@ -2,51 +2,46 @@
 import './Social.css';
 
 // Icons
-import {
-    BsLinkedin,
-    BsGithub,
-    BsPhoneFill,
-    BsEnvelopeFill,
-} from 'react-icons/bs';
+import { BsLinkedin, BsGithub, BsPhoneFill, BsEnvelopeFill } from 'react-icons/bs';
+
+const linkedin = 'https://www.linkedin.com/in/leightongrant/';
+const github = 'https://github.com/leightongrant';
 
 function Social() {
-    return (
-        <div className=''>
-            <div className='social-links  d-inline-flex gap-2'>
-                <a href='https://github.com/misterouija/'>
-                    <BsGithub />
-                </a>
-                <a href='https://www.linkedin.com/in/leighton-grant-009483116/'>
-                    <BsLinkedin />
-                </a>
-                <a href='mailto:oneal.grant@outlook.com'>
-                    <BsEnvelopeFill />
-                </a>
-                <a href='tel:07886028826'>
-                    <BsPhoneFill />
-                </a>
-            </div>
-        </div>
-    );
+  return (
+    <div className=''>
+      <div className='social-links  d-inline-flex gap-2'>
+        <a href={github} target='_blank'>
+          <BsGithub />
+        </a>
+        <a href={linkedin} target='_blank'>
+          <BsLinkedin />
+        </a>
+        <a href='mailto:oneal.grant@outlook.com'>
+          <BsEnvelopeFill />
+        </a>
+        <a href='tel:07886028826'>
+          <BsPhoneFill />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 function LinkedIn() {
-    return (
-        <a
-            href='https://www.linkedin.com/in/leighton-grant-009483116/'
-            className='social-button'
-        >
-            <BsLinkedin />
-        </a>
-    );
+  return (
+    <a href={linkedin} target='_blank' className='social-button'>
+      <BsLinkedin />
+    </a>
+  );
 }
 
 function GitHub() {
-    return (
-        <a href='https://github.com/misterouija/' className='social-button'>
-            <BsGithub />
-        </a>
-    );
+  return (
+    <a href={github} target='_blank' className='social-button'>
+      <BsGithub />
+    </a>
+  );
 }
 
 export { Social, LinkedIn, GitHub };
