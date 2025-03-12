05ec78d6a8603955bc5142f862dcbae158232a5c
Leighton Grant <oneal.grant@outlook.com>
2025-03-12 22:20:02 +0000

    Refactor Footer and PageBanner components: update copyright year dynamically and improve code formatting

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,16 @@
 import './Footer.css'
 const Footer = () => {
-  return (
-    <footer id="footer" className="py-3">
-      <div className="container d-flex justify-content-center align-items-center">
-        <div className="copyright">
-          <small>© Copyright 2024</small> <strong>Leighton Grant</strong>
-        </div>
-      </div>
-    </footer>
-  )
+	const year = new Date().getFullYear()
+	return (
+		<footer id='footer' className='py-3'>
+			<div className='container d-flex justify-content-center align-items-center'>
+				<div className='copyright fs-5'>
+					<small className='fw-normal'>Leighton Grant</small>
+					<small> © Copyright {year}</small>
+				</div>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
--- src/components/pagebanner/PageBanner.jsx
+++ src/components/pagebanner/PageBanner.jsx
@@ -1,19 +1,19 @@
 export default function PageBanner({ pageTitle, bannerBg }) {
-  const bgStyles = {
-    backgroundImage: `url(${bannerBg})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'bottom',
-  }
-  const overlay = {
-    backgroundColor: 'rgba(0,0,0,0.7)',
-  }
-  return (
-    <section className="page-banner">
-      <div className="" style={bgStyles}>
-        <div className="padding-lg banner-padding" style={overlay}>
-          <h2 className="text-center text-light">{pageTitle}</h2>
-        </div>
-      </div>
-    </section>
-  )
+	const bgStyles = {
+		backgroundImage: `url(${bannerBg})`,
+		backgroundSize: 'cover',
+		backgroundPosition: 'bottom',
+	}
+	const overlay = {
+		backgroundColor: 'rgba(0,0,0,0.7)',
+	}
+	return (
+		<section className='page-banner'>
+			<div className='' style={bgStyles}>
+				<div className='padding-lg banner-padding' style={overlay}>
+					<h2 className='text-center text-light'>{pageTitle}</h2>
+				</div>
+			</div>
+		</section>
+	)
 }
