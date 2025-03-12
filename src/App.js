6698af9bbcfac2a61b079d963baf4933787145b7
Leighton Grant <oneal.grant@outlook.com>
2025-03-12 17:24:17 +0000

    Refactor App.js to improve code readability and remove unused SiteAdmin import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// Routes
 import { Outlet, BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // Pages
@@ -6,7 +5,6 @@ import About from './pages/about/About'
 import Projects from './pages/projects/Projects'
 import Contact from './pages/contact/Contact'
 import ProjectDetails from './pages/projects/ProjectDetails'
-import SiteAdmin from './pages/admin/SiteAdmin'
 import NotFound from './pages/notfound/NotFound'
 
 // Components
@@ -23,79 +21,78 @@ import { supabase } from './utils/supabase'
 
 // Layout
 function Layout() {
-  const [bootcampProjects, setBootcampProjects] = useState({
-    data: null,
-    error: null,
-    status: null,
-    selected: '',
-    mode: '',
-    session: null,
-  })
+	const [bootcampProjects, setBootcampProjects] = useState({
+		data: null,
+		error: null,
+		status: null,
+		selected: '',
+		mode: '',
+		session: null,
+	})
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setBootcampProjects((obj) => ({ ...obj, session: session }))
-    })
+	useEffect(() => {
+		supabase.auth.getSession().then(({ data: { session } }) => {
+			setBootcampProjects(obj => ({ ...obj, session: session }))
+		})
 
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setBootcampProjects((obj) => ({ ...obj, session: session }))
-    })
+		const {
+			data: { subscription },
+		} = supabase.auth.onAuthStateChange((_event, session) => {
+			setBootcampProjects(obj => ({ ...obj, session: session }))
+		})
 
-    return () => subscription.unsubscribe()
-  }, [])
+		return () => subscription.unsubscribe()
+	}, [])
 
-  useEffect(() => {
-    try {
-      async function fetchProjects() {
-        const res = await supabase.from('bootcamp').select()
-        if (res.status === 200) {
-          setBootcampProjects((obj) => ({ ...obj, data: res.data }))
-        }
-        if (res.error) {
-          setBootcampProjects((obj) => ({ ...obj, error: res.error }))
-        }
-      }
-      fetchProjects()
-    } catch (e) {
-      console.log(e)
-    } finally {
-      setBootcampProjects((obj) => ({ ...obj, status: null }))
-    }
-  }, [bootcampProjects.status])
-  return (
-    <>
-      <Header
-        bootcampProjects={bootcampProjects}
-        setBootcampProjects={setBootcampProjects}
-      />
-      <Outlet context={[bootcampProjects, setBootcampProjects]} />
-      <AddProject
-        bootcampProjects={bootcampProjects}
-        setBootcampProjects={setBootcampProjects}
-      />
-      <Login bootcampProjects={bootcampProjects} />
-      <Footer />
-    </>
-  )
+	useEffect(() => {
+		try {
+			async function fetchProjects() {
+				const res = await supabase.from('bootcamp').select()
+				if (res.status === 200) {
+					setBootcampProjects(obj => ({ ...obj, data: res.data }))
+				}
+				if (res.error) {
+					setBootcampProjects(obj => ({ ...obj, error: res.error }))
+				}
+			}
+			fetchProjects()
+		} catch (e) {
+			console.log(e)
+		} finally {
+			setBootcampProjects(obj => ({ ...obj, status: null }))
+		}
+	}, [bootcampProjects.status])
+	return (
+		<>
+			<Header
+				bootcampProjects={bootcampProjects}
+				setBootcampProjects={setBootcampProjects}
+			/>
+			<Outlet context={[bootcampProjects, setBootcampProjects]} />
+			<AddProject
+				bootcampProjects={bootcampProjects}
+				setBootcampProjects={setBootcampProjects}
+			/>
+			<Login bootcampProjects={bootcampProjects} />
+			<Footer />
+		</>
+	)
 }
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Hero />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/projects/:id" element={<ProjectDetails />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/thanks" element={<Thanks />} />
-          <Route path="/*" element={<NotFound />} />
-        </Route>
-        <Route path="/admin/*" element={<SiteAdmin />} />
-      </Routes>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route element={<Layout />}>
+					<Route path='/' element={<Hero />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/projects' element={<Projects />} />
+					<Route path='/projects/:id' element={<ProjectDetails />} />
+					<Route path='/contact' element={<Contact />} />
+					<Route path='/thanks' element={<Thanks />} />
+					<Route path='/*' element={<NotFound />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	)
 }
