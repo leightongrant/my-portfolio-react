ffbb24175c31c4cc38456228a89112388725a910
Leighton Grant <oneal.grant@outlook.com>
2025-03-14 11:46:00 +0000

    Refactor Header component to remove LinkContainer and use anchor tags for navigation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,6 @@ import { Social } from '../social/Social'
 import { IconContext } from 'react-icons'
 import { HiMenu, HiOutlineX } from 'react-icons/hi'
 import logo from '../../assets/lg-logo.png'
-import { LinkContainer } from 'react-router-bootstrap'
 import { useState } from 'react'
 import clsx from 'clsx'
 import { useResolvedPath } from 'react-router'
@@ -22,41 +21,33 @@ const Header = ({ bootcampProjects, setBootcampProjects }) => {
 			<nav className='navbar navbar-expand-lg'>
 				<div className='container-fluid d-flex justify-content-between align-items-center'>
 					<div>
-						<LinkContainer to='/'>
-							<a className='navbar-brand' href='/'>
-								<img
-									src={logo}
-									alt='leighton grant logo'
-									width={53}
-									height={35}
-								/>
-							</a>
-						</LinkContainer>
+						<a className='navbar-brand' href='/'>
+							<img
+								src={logo}
+								alt='leighton grant logo'
+								width={53}
+								height={35}
+							/>
+						</a>
 					</div>
 
 					<div className='collapse navbar-collapse flex-grow-0' id='main-nav'>
 						<div className='navbar-nav gap-lg-4 NavLinks'>
-							<LinkContainer to='/'>
-								<a className='nav-link' aria-current='page' href='/'>
-									Home
-								</a>
-							</LinkContainer>
-							<LinkContainer to='/about'>
-								<a className='nav-link' href='/about'>
-									About
-								</a>
-							</LinkContainer>
-							<LinkContainer to='/projects' data-bs-toggle='show'>
-								<a className='nav-link' href='/projects'>
-									Projects
-								</a>
-							</LinkContainer>
+							<a className='nav-link' aria-current='page' href='/'>
+								Home
+							</a>
 
-							<LinkContainer to='/contact'>
-								<a className='nav-link' href='/contact'>
-									Contact
-								</a>
-							</LinkContainer>
+							<a className='nav-link' href='/about'>
+								About
+							</a>
+
+							<a className='nav-link' href='/projects' data-bs-toggle='show'>
+								Projects
+							</a>
+
+							<a className='nav-link' href='/contact'>
+								Contact
+							</a>
 						</div>
 					</div>
 
