63e179777229397a085d749861cac784a7e892ba
Leighton Grant <oneal.grant@outlook.com>
2025-03-18 20:20:01 +0000

    Add accessibility features to social links components with descriptive props

--- src/components/social/index.jsx
+++ src/components/social/index.jsx
@@ -9,32 +9,71 @@ import { Link } from 'react-router'
 const linkedin = 'https://www.linkedin.com/in/leightongrant/'
 const github = 'https://github.com/leightongrant'
 
-function Phone({ className }) {
+/**
+ * A link to my phone number.
+ *
+ * @param {Object} props
+ * @prop {string} className - a CSS class name to apply to the component.
+ * @prop {ReactNode} children - a child component to render inside the link.
+ *
+ * @returns {ReactElement}
+ */
+function Phone({ className, children }) {
 	return (
 		<Link to='tel:+447886028826' className={className}>
-			<BsPhoneFill />
+			<BsPhoneFill /> <span className='ms-2 fs-6'>{children}</span>
 		</Link>
 	)
 }
 
-function Email({ className }) {
+/**
+ * A link to my email address.
+ *
+ * @param {Object} props
+ * @prop {string} className - a CSS class name to apply to the component.
+ * @prop {ReactNode} children - a child component to render inside the link.
+ *
+ * @returns {ReactElement}
+ */
+function Email({ className, children }) {
 	return (
 		<Link to='mailto:dev@leightongrant.me' className={className}>
-			<BsEnvelopePaperFill />
+			<BsEnvelopePaperFill /> <span className='ms-2 fs-6'>{children}</span>
 		</Link>
 	)
 }
-function LinkedIn({ className }) {
+
+/**
+ * A link to my LinkedIn profile.
+ *
+ * @param {Object} props
+ * @prop {string} className - a CSS class name to apply to the component.
+ * @prop {ReactNode} children - a child component to render inside the link.
+ *
+ * @returns {ReactElement}
+ */
+
+function LinkedIn({ className, children }) {
 	return (
 		<Link to={linkedin} target='_blank' className={className} rel='noreferrer'>
-			<BsLinkedin />
+			<BsLinkedin /> <span className='ms-2 fs-6'>{children}</span>
 		</Link>
 	)
 }
-function GitHub({ className }) {
+
+/**
+ * A link to my GitHub profile.
+ *
+ * @param {Object} props
+ * @prop {string} className - a CSS class name to apply to the component.
+ * @prop {ReactNode} children - a child component to render inside the link.
+ *
+ * @returns {ReactElement}
+ */
+function GitHub({ className, children }) {
 	return (
 		<Link to={github} target='_blank' className={className} rel='noreferrer'>
-			<BsGithub />
+			<BsGithub /> <span className='ms-2 fs-6'>{children}</span>
 		</Link>
 	)
 }
