55c5d8cde4b86661edfaefa0b97341319e40eed3
Leighton Grant <oneal.grant@outlook.com>
2025-03-27 12:22:19 +0000

    Refactor ProjectDetails component layout to use Row and Col for improved responsiveness

--- src/pages/projects/ProjectDetails.jsx
+++ src/pages/projects/ProjectDetails.jsx
@@ -10,6 +10,8 @@ import Stack from 'react-bootstrap/Stack'
 import Image from 'react-bootstrap/Image'
 import { Container } from 'react-bootstrap'
 import Breadcrumbs from '../../components/breadcrumb'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function Details(props) {
 	const navigate = useNavigate()
@@ -17,52 +19,56 @@ function Details(props) {
 	return (
 		<main className='section-padding'>
 			<Container>
-				<Stack gap={5}>
-					<Stack>
-						<Image
-							src={props.img}
-							className='border p-0 img-fluid shadow rounded'
-							alt={props.title}
-							width={640}
-							height={480}
-						/>
-					</Stack>
-					<Stack>
-						<h4 className='my-3'>Description</h4>
-						<p className='card-text' style={{ textWrap: 'balance' }}>
-							{props.description}
-						</p>
-					</Stack>
-					<Stack>
-						<h4 className='my-3'>Links</h4>
-						<Stack direction='horizontal'>
-							<Link
-								to={props.app_url}
-								target='_blank'
-								rel='noreferrer'
-								className='btn btn-sm projectBtn'
-							>
-								App <RiExternalLinkFill />
-							</Link>
-							<Link
-								to={props.repo_url}
-								target='_blank'
-								rel='noreferrer'
-								className='btn btn-sm projectBtn ms-2'
-							>
-								Repo <RiExternalLinkFill />
-							</Link>
-
-							<Button
-								type='button'
-								className='btn btn-sm projectBtn ms-2 border-0'
-								onClick={() => navigate('/projects')}
-							>
-								<HiArrowNarrowLeft /> Back to Projects
-							</Button>
+				<Row className='row-cols-1 row-cols-lg-2 g-5'>
+					<Col>
+						<Stack>
+							<Image
+								src={props.img}
+								className='border p-0 img-fluid shadow rounded'
+								alt={props.title}
+								width={640}
+								height={480}
+							/>
 						</Stack>
-					</Stack>
-				</Stack>
+					</Col>
+					<Col>
+						<Stack>
+							<h4 className='mb-3'>Description</h4>
+							<p className='card-text' style={{ textWrap: 'balance' }}>
+								{props.description}
+							</p>
+						</Stack>
+						<Stack>
+							<h4 className='my-3'>Links</h4>
+							<Stack direction='horizontal'>
+								<Link
+									to={props.app_url}
+									target='_blank'
+									rel='noreferrer'
+									className='btn btn-sm projectBtn'
+								>
+									App <RiExternalLinkFill />
+								</Link>
+								<Link
+									to={props.repo_url}
+									target='_blank'
+									rel='noreferrer'
+									className='btn btn-sm projectBtn ms-2'
+								>
+									Repo <RiExternalLinkFill />
+								</Link>
+
+								<Button
+									type='button'
+									className='btn btn-sm projectBtn ms-2 border-0'
+									onClick={() => navigate('/projects')}
+								>
+									<HiArrowNarrowLeft /> Back to Projects
+								</Button>
+							</Stack>
+						</Stack>
+					</Col>
+				</Row>
 			</Container>
 		</main>
 	)
