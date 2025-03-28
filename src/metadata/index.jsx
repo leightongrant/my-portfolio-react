eb4e60609babfe494e5c11a41fd87466a2f309da
Leighton Grant <oneal.grant@outlook.com>
2025-03-28 17:26:24 +0000

    Add ContactMetadata component for improved SEO on contact page

---
+++ src/metadata/index.jsx
@@ -0,0 +1,9 @@
+export const ContactMetadata = () => {
+	return (
+		<>
+			<title>Leighton Grant's Portfolio | Contact</title>
+			<meta name='description' content='Contact Leighton Grant here.' />
+			<meta name='keywords' content='HTML, CSS, JavaScript, React, Portfolio' />
+		</>
+	)
+}
