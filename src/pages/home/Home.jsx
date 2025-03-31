726b2c0d038c955d66f1ddcfab927560f632ae5c
Leighton Grant <oneal.grant@outlook.com>
2025-03-31 20:32:12 +0100

    Add Open Graph image and alt text to Home page metadata for enhanced SEO

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,6 +8,8 @@ const Home = () => {
 				description='A portfolio built using React to showcase some of the projects I have completed during my Frontend WebDeveloper Bootcamp.'
 				keywords='Frontend Web Development, React, JavaScript, HTML, CSS, Responsive Design, User Experience (UX), Web Applications, Portfolio, Web Developer'
 				canonical='https://leightongrant.me'
+				image='https://leightongrant.me/og-image.webp'
+				imageAlt='Leighton Grant Portfolio'
 			/>
 			<Hero />
 		</>
