9ef3271c0cbba1db51e32a21fe72edb411d53616
Leighton Grant <oneal.grant@outlook.com>
2025-04-02 13:55:20 +0100

    Refactor placeholders for improved layout and accessibility

--- src/components/placeholders/index.jsx
+++ src/components/placeholders/index.jsx
@@ -1,26 +1,32 @@
 import { LuServerOff } from 'react-icons/lu'
 import Container from 'react-bootstrap/Container'
 import Spinner from 'react-bootstrap/Spinner'
+import Row from 'react-bootstrap/Row'
 export const ServerError = () => {
 	return (
-		<div className='py-5'>
-			<div className='container h-75 d-flex flex-column align-items-center justify-content-center gap-5'>
-				<LuServerOff style={{ fontSize: '4em' }} />
-				<h5 style={{ textWrap: 'balance' }} className='text-center display-6'>
-					There was a problem with the server. Please try again later.
-				</h5>
-			</div>
-		</div>
+		<section className='section-padding'>
+			<Container>
+				<Row className='h-75 flex-column align-items-center justify-content-center gap-5'>
+					<LuServerOff style={{ fontSize: '4em' }} />
+					<h6
+						style={{ textWrap: 'balance' }}
+						className='text-center display-6 fs-6'
+					>
+						There was a problem with the server. Please try again later.
+					</h6>
+				</Row>
+			</Container>
+		</section>
 	)
 }
 
 export const Loading = () => {
 	return (
-		<section className='section-padding h-100'>
-			<Container className='align-items-center justify-content-center d-flex h-100'>
-				<Spinner animation='border' variant='secondary'>
-					<span className='visually-hidden'>Loading...</span>
-				</Spinner>
+		<section className='section-padding'>
+			<Container>
+				<Row className='align-items-center justify-content-center'>
+					<Spinner animation='border' variant='secondary' />
+				</Row>
 			</Container>
 		</section>
 	)
