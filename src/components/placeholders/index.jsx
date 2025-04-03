9f3c80c9fb9803016f18742542eef9a9d815a749
Leighton Grant <oneal.grant@outlook.com>
2025-04-03 09:39:47 +0100

    Add ProjectsSkeleton component for loading state representation

--- src/components/placeholders/index.jsx
+++ src/components/placeholders/index.jsx
@@ -2,6 +2,11 @@ import { LuServerOff } from 'react-icons/lu'
 import Container from 'react-bootstrap/Container'
 import Spinner from 'react-bootstrap/Spinner'
 import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Placeholder from 'react-bootstrap/Placeholder'
+import Col from 'react-bootstrap/Col'
+
 export const ServerError = () => {
 	return (
 		<section className='section-padding'>
@@ -26,6 +31,34 @@ export const Loading = () => {
 			<Container>
 				<Row className='align-items-center justify-content-center'>
 					<Spinner animation='border' variant='secondary' />
+				</Row>
+			</Container>
+		</section>
+	)
+}
+
+export function ProjectsSkeleton() {
+	return (
+		<section className='section-padding'>
+			<Container>
+				<Row className='row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4'>
+					{Array.from({ length: 8 }).map((_, index) => (
+						<Col key={index}>
+							<Card>
+								<Card.Body>
+									<Placeholder as={Card.Title} animation='glow'>
+										<Placeholder xs={6} />
+									</Placeholder>
+									<Placeholder as={Card.Text} animation='glow'>
+										<Placeholder xs={7} /> <Placeholder xs={4} />{' '}
+										<Placeholder xs={4} /> <Placeholder xs={6} />{' '}
+										<Placeholder xs={8} />
+									</Placeholder>
+									<Placeholder.Button variant='secondary' xs={6} />
+								</Card.Body>
+							</Card>
+						</Col>
+					))}
 				</Row>
 			</Container>
 		</section>
