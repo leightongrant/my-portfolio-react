85d73ef06c405e5bcb7929a6d389641a3e21a1e6
Leighton Grant <oneal.grant@outlook.com>
2025-04-04 07:02:50 +0100

    Add user menu dropdown to SignIn component for improved user experience

--- src/components/signin/index.jsx
+++ src/components/signin/index.jsx
@@ -7,20 +7,32 @@ import Tooltip from 'react-bootstrap/Tooltip'
 import { useModalStore } from '../../lib/zustand'
 import { useAuthStore } from '../../lib/zustand'
 import { BiSolidUser } from 'react-icons/bi'
+import Dropdown from 'react-bootstrap/Dropdown'
+
+function UserMenu({ children, session }) {
+	let {
+		user: { email },
+	} = session
+	return (
+		<Dropdown align={{ md: 'end' }}>
+			<Dropdown.Toggle variant='transparent'>{children}</Dropdown.Toggle>
+			<Dropdown.Menu>
+				<Dropdown.Item>User: {email}</Dropdown.Item>
+				<Dropdown.Divider />
+				<Dropdown.Item onClick={() => supabaseClient.auth.signOut()}>
+					<IconContext.Provider value={{ className: 'logoutBtn fs-3 ' }}>
+						<FaSignOutAlt /> Sign Out
+					</IconContext.Provider>
+				</Dropdown.Item>
+			</Dropdown.Menu>
+		</Dropdown>
+	)
+}
 
 const SignIn = () => {
 	const showModal = useModalStore(state => state.showModal)
 	const setMode = useModalStore(state => state.setMode)
 	const session = useAuthStore(state => state.session)
-
-	if (session) {
-		console.log(session)
-	}
-
-	async function signOut() {
-		await supabaseClient.auth.signOut()
-	}
-
 	async function signIn() {
 		setMode('login')
 		showModal()
@@ -34,18 +46,13 @@ const SignIn = () => {
 
 	if (session) {
 		return (
-			<OverlayTrigger
-				placement='bottom'
-				delay={{ show: 250, hide: 400 }}
-				overlay={renderTooltip}
-			>
-				<Stack className='align-items-right justify-content-center'>
+			<Stack className='align-items-right justify-content-center'>
+				<UserMenu session={session}>
 					<IconContext.Provider value={{ className: 'logoutBtn fs-3 ' }}>
-						{/* <FaSignOutAlt onClick={signOut} /> */}
-						<BiSolidUser onClick={signOut} />
+						<BiSolidUser />
 					</IconContext.Provider>
-				</Stack>
-			</OverlayTrigger>
+				</UserMenu>
+			</Stack>
 		)
 	}
 
